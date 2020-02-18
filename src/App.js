0c33ee252a7c28f6314a3c1d482f5fc9dea1c389
Roman Danilov <r.danylov@gmail.com>
2020-02-19 00:15:16 +0200

    Datasource

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="http://127.0.0.1:8000/workspace/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Datasource
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
